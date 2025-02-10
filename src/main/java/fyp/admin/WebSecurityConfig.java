979de592d39682663caa2ea79615e287ad6179d2
ruiienn <22010118@myrp.edu.sg>
2025-02-10 08:07:52 +0800

    new update

--- src/main/java/fyp/admin/WebSecurityConfig.java
+++ src/main/java/fyp/admin/WebSecurityConfig.java
@@ -13,43 +13,47 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new MemberDetailsService();
-    }
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new MemberDetailsService();
+	}
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder()); 
-        return authProvider;
-    }
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                .requestMatchers("/rewards/add", "/rewards/edit/*", "/rewards/delete/*", "/rewards/save",
-                        "/activities/add", "/activities/edit/*", "/activities/delete/*", "/activities/save", "/members",
-                        "/members/add", "/members/edit/*", "/members/delete/*", "/members/save", "/login")
-                .hasRole("ADMIN")
-                .requestMatchers("/", "/leaderboard", "/history", "/rewards", "/activities", "/more", "/avatar", "/profile",
-                        "/guide", "/feedback", "/forget","/changePassword","/sendOtp","/verifyOtp","/images/*")
-                .permitAll()
-                .anyRequest().authenticated()
-        )
-        .formLogin((login) -> login.loginPage("/login")
-                .permitAll()
-                .defaultSuccessUrl("/", true)
-                .failureUrl("/login?error=true"))  // Ensures error is passed correctly
-        .logout((logout) -> logout.logoutSuccessUrl("/"))
-        .exceptionHandling((exceptionHandling) -> exceptionHandling.accessDeniedPage("/403"));
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+				.requestMatchers("/rewards/add", "/rewards/edit/*", "/rewards/delete/*", "/rewards/save",
+						"/activities/add", "/activities/edit/*", "/activities/delete/*", "/activities/save", "/members",
+						"/members/add", "/members/edit/*", "/members/delete/*", "/members/save", "/login")
+				.hasRole("ADMIN")
+				.requestMatchers("/", "/leaderboard", "/history", "/rewards", "/activities", "/more", "/avatar",
+						"/profile", "/guide", "/feedback", "/forget", "/changePassword", "/sendOtp", "/verifyOtp",
+						"/images/*")
+				.permitAll() // Home page is visible without logging in
+				.requestMatchers("/bootstrap/*/*").permitAll() // for static resources, visible to all
+				.requestMatchers("/images/**").permitAll() // for static resources, visible to all
+				.requestMatchers("/uploads/**").permitAll()// for upload images, visible to all
+				.requestMatchers("/avatar/**").permitAll()// for avatar images, visible to all
+				.anyRequest().authenticated())
+				.formLogin((login) -> login.loginPage("/login").permitAll().defaultSuccessUrl("/", true)
+						.failureUrl("/login?error=true")) // Ensures error is passed correctly
+				.logout((logout) -> logout.logoutSuccessUrl("/"))
+				.exceptionHandling((exceptionHandling) -> exceptionHandling.accessDeniedPage("/403"));
 
-        return http.build();
-    }
+		return http.build();
+	}
 }
+
+
